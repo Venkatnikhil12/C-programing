29404d9be8c3aa02479d86938f36702bdfe71a49
Venkatnikhil12 <chittornikhil18@gmail.com>
2024-10-26 15:13:14 +0530

    Add files via upload

---
+++ venkat nikhil/add of two numbers - Copy.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	int x=4,y=5,sum;
+	sum=x+y;
+	printf("the sum of two numbers is %d",sum);
+}
---
+++ venkat nikhil/decrement operation.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+int x=10,y=10,z;
+z=x--;
+printf("z=%dx=%d\n",z,x);
+z=--y;
+printf("z=%dy=%d",z,y);
+}
