66af56175ceae5adb09d9ef2e8b6cd9cab9456c9
Venkatnikhil12 <chittornikhil18@gmail.com>
2024-10-26 15:42:39 +0530

    Add files via upload

---
+++ relation operator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	printf("4>5:%d\n",4>5);
+	printf("4>=5:%d\n",4>=5);
+	printf("4<5:%d\n",4<5);
+	printf("4<=5:%d\n",4<=5);
+	printf("4==5:%d\n",4==5);
+	printf("4!=5:%d",4!=5);
+}
