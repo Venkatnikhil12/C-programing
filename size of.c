6aba877a54af0e73353f2015d17b41a4a37d8a07
Venkatnikhil12 <chittornikhil18@gmail.com>
2024-10-29 09:18:49 +0530

    Add files via upload

---
+++ size of.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int x;
+	float y;
+	x=1000;
+	y=800.0;
+	printf("size of x=%d\n",sizeof (x));
+	printf("size of y=%d\n",sizeof(y));
+	getch();
+}
